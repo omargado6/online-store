f8bd80a013a4f762966696bc3291d40c3f6a46b1
memo <oo.gmail.com>
2024-05-01 10:11:13 +0300

    first commit

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,12 +1,14 @@
 import React from 'react'
-
+import laptop from"../images/home/laptop.jpg"
 function ProductDetails() {
   return (
     <div className="productDetails">
-        <div className="w-11/12 flex justify-around gap-4 m-auto text-center">
-          <div className="w-1/6 max">asd</div>
-          <div className="w-3/6">das</div>
-          <div className="w-2/6">sda</div>
+        <div className="w-11/12 flex flex-wrap justify-around gap-4 m-auto pt-5 text-center">
+          <div className="w-3/12 max-sm:w-full">
+            <img src={laptop} alt="laptop" />
+          </div>
+          <div className="w-4/12 h-96 max-sm:w-full">das</div>
+          <div className="w-4/12 max-sm:w-full">sda</div>
         </div>
     </div>
   )
